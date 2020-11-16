5aec9bab6bf3613ad8b6b3e068bdc3df30945507
Jaime Reyes Cárdenas <jaimefabianreyescardenas@gmail.com>
2020-11-15 20:05:58 -0400

    Proyecto creado

---
+++ AmoresCaninos/src/main/java/cl/recoders/amorescaninos/AmoresCaninosApplication.java
@@ -0,0 +1,13 @@
+package cl.recoders.amorescaninos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmoresCaninosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmoresCaninosApplication.class, args);
+	}
+
+}
---
+++ AmoresCaninos/src/test/java/cl/recoders/amorescaninos/AmoresCaninosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.recoders.amorescaninos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmoresCaninosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
